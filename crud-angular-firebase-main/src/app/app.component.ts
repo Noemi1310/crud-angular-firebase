43eb3209964c3029aeec46a474e47e47ef8f1a4f
Noemi1310 <110252722+Noemi1310@users.noreply.github.com>
2023-06-27 22:15:19 -0300

    commit

---
+++ crud-angular-firebase-main/src/app/app.component.ts
@@ -0,0 +1,35 @@
+
+import { Component } from '@angular/core';
+
+
+
+
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+
+
+
+
+@Component({
+
+  selector: 'app-root',
+
+  templateUrl: './app.component.html',
+
+  styleUrls: ['./app.component.css']
+
+})
+
+export class AppComponent {
+
+  title = 'crud-angular-firebase';
+
+
+
+  constructor(public db: AngularFireDatabase) {
+
+
+
+  }
+
+}
+
---
+++ crud-angular-firebase-main/src/environment.ts
@@ -0,0 +1,15 @@
+
+export const environment = {
+
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyCjIIlgzbhj4disArNnO1C9L7Ypbr8leik",
+    authDomain: "crud-projeto-angular.firebaseapp.com",
+    databaseURL: "https://crud-projeto-angular-default-rtdb.firebaseio.com",
+    projectId: "crud-projeto-angular",
+    storageBucket: "crud-projeto-angular.appspot.com",
+    messagingSenderId: "651063568307",
+    appId: "1:651063568307:web:6f82ebc91d390fed81cdc6"
+  }
+};
